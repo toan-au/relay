f209e4ac9320e75fc0a85044f6a6ef34aa343917
toan_au <toanau.dev@gmail.com>
2026-03-05 23:55:01 +1100

    feat: Connected PostgreSQL, MinIO/S3, and started work on upload endpoint

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,15 +1,48 @@
 use axum::{
+    extract::DefaultBodyLimit,
     routing::{get, post},
     Router,
 };
+use sqlx::PgPool;
 
 mod routes;
 
+// 1 gb max file upload
+const MAX_VIDEO_UPLOAD_SIZE: usize = 1024 * 1024 * 1024;
+
+#[derive(Clone)]
+pub struct AppState {
+    pub db: PgPool,
+    pub s3: aws_sdk_s3::Client,
+    pub bucket: String,
+}
+
 #[tokio::main]
 async fn main() {
+    // Load .env in dev
+    #[cfg(debug_assertions)]
+    dotenvy::dotenv().ok();
+
+    // Setup AppState
+    let database_url = std::env::var("DATABASE_URL").expect("Unable to find DB endpoint");
+    let db = PgPool::connect(&database_url)
+        .await
+        .expect("Unable to connect to DB");
+
+    let config = aws_config::load_from_env().await;
+    let s3 = aws_sdk_s3::Client::new(&config);
+    let bucket = "videos".to_string();
+
+    let app_state = AppState { db, s3, bucket };
+
+    // Setup routes
     let app: Router = Router::new()
         .route("/healthcheck", get(health_check))
-        .route("/api/videos", post(routes::videos::upload_video));
+        .route(
+            "/api/videos",
+            post(routes::videos::upload_video).layer(DefaultBodyLimit::max(MAX_VIDEO_UPLOAD_SIZE)),
+        )
+        .with_state(app_state);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
--- backend/src/routes/videos.rs
+++ backend/src/routes/videos.rs
@@ -1,1 +1,34 @@
-pub async fn upload_video() {}
+use axum::{
+    body::Bytes,
+    extract::Multipart,
+    http::StatusCode,
+    response::{IntoResponse, Response},
+};
+
+struct File {
+    file_name: String,
+    content_type: String,
+    bytes: Bytes,
+}
+
+pub async fn upload_video(mut multipart: Multipart) -> Result<Response, Response> {
+    while let Some(field) = multipart
+        .next_field()
+        .await
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR.into_response())?
+    {
+        if let Some("video") = field.name() {
+            println!("{:?} {:?}", field.file_name(), field.content_type());
+        }
+
+        let file_name = field.file_name().unwrap_or_default().to_owned();
+        let content_type = field.content_type().unwrap_or_default().to_owned();
+
+        let bytes = field
+            .bytes()
+            .await
+            .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR.into_response())?;
+    }
+
+    Ok(StatusCode::OK.into_response())
+}
