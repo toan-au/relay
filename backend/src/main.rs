6d2188bf96912fa861029bcab5be2c285e87dcb6
toan_au <toanau.dev@gmail.com>
2026-03-05 20:16:45 +1100

    chore: Added more boiler plate for  video routes module

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,8 +1,15 @@
-use axum::{routing::get, Router};
+use axum::{
+    routing::{get, post},
+    Router,
+};
+
+mod routes;
 
 #[tokio::main]
 async fn main() {
-    let app: Router = Router::new().route("/healthcheck", get(health_check));
+    let app: Router = Router::new()
+        .route("/healthcheck", get(health_check))
+        .route("/api/videos", post(routes::videos::upload_video));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
